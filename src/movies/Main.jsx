fbe31162ac90dcdd27b29ba81755422cfff3a4f5
Srikrishna <krishna333sp@gmail.com>
2023-02-15 12:39:27 +0530

    categorywise updated

--- src/movies/Main.jsx
+++ src/movies/Main.jsx
@@ -6,6 +6,9 @@ import Pagination from "./pagination";
 var API_key = "&api_key=7c01b82b0aa395aad5febf7f163923a8";
 var base_URL = "https://api.themoviedb.org/3";
 var url = base_URL + "/discover/movie?sort_by=popularity.desc" + API_key;
+var top_rated_movie = base_URL + "/discover/movie/?certification_country=US&certification=R&sort_by=vote_average.desc" + API_key;
+var upcoming_movie = base_URL + "/discover/movie?primary_release_date.gte=2022-11-30" + API_key;
+var arrData = ["Popular", "Top Rated", "Upcoming"]
 
 function Main() {
     const [movieData, setMovieData] = useState([]);
@@ -31,6 +34,17 @@ function Main() {
         })
     }, [url_set])
 
+    const getData = (movieType) => {
+        if (movieType === "Popular")
+            url = base_URL + "/discover/movie?sort_by=popularity.desc" + API_key;
+        if (movieType === "Top Rated") {
+            url = top_rated_movie;
+        }
+        if (movieType === "Upcoming")
+            url = upcoming_movie;
+        setURL(url)
+    }
+
     function SearchResult(event) {
         event.preventDefault()
         url = base_URL + "/search/movie?api_key=7c01b82b0aa395aad5febf7f163923a8&query=" + search;
@@ -40,10 +54,20 @@ function Main() {
 
     return (
         <>
-            
+
             <div className="header">
                 <h3>MovieDb</h3>
-
+                <nav>
+                    <ul>
+                        {
+                            arrData.map((value) => {
+                                return (
+                                    <li><a href="#" name={value} onClick={(e) => { getData(e.target.name) }}>{value}</a></li>
+                                )
+                            })
+                        }
+                    </ul>
+                </nav>
                 <form>
                     <div className="searchBox">
                         {/* onKeyPress={searchMovie} */}
